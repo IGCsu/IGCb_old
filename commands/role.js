9b75fc0b3816ae25d66e84455518bcb393965c45
BVN4 <Ruslan2804y@gmail.com>
2021-04-27 15:51:31 +0300

    release /commands/role.js

--- commands/role.js
+++ commands/role.js
@@ -17,7 +17,7 @@ module.exports = {
    * @param {Message} msg
    * @param {Array}   params Параметры команды
    */
-  call : function(msg, params){
+  call : async function(msg, params){
     const permission = this.permission(msg);
     let users = [];
     let role = '';
@@ -32,22 +32,24 @@ module.exports = {
     // Отправка списка доступных игровых ролей
     if(!role.length) return this.help(msg);
 
-    let finded = this.has(msg, role);
+    let finded = await this.has(msg, role);
 
     if(!finded.role){
-      if(!permission) return this.error(msg);
-      finded = this.create(msg, role, finded.position);
+      if(!permission)
+        return this.error(msg, 'Роль с названием "' + role + '" не найдена');
+      finded = await this.create(msg, role, finded.position);
     }
 
     // Переключение роли пользователю команды
     if(!users.length || users[0] == msg.author.id)
-      return this.toggle(msg, role);
+      return this.toggle(msg, finded.role, msg.author.id);
 
     // Провека наличия прав на выдачу роли
-    if(!permission) return this.error(msg);
+    if(!permission)
+      return this.error(msg, 'У вас недостаточно прав для изменения ролей других пользователей');
 
     // Переключение роли указанным юзерам
-    users.forEach(user => this.toggle(msg, role, user));
+    users.forEach(user => this.toggle(msg, finded.role, user));
   },
 
 
@@ -73,14 +75,13 @@ module.exports = {
 
   /**
    * Прикрепляет эмодзи ошибки к сообщению пользователя
-   * и отправляет help и список доступных игровых ролей
+   * и отправляет информацию об ошибке
    *
    * @param {Message} msg
+   * @param {String}  name Текст ошибки
    */
-  error : function(msg){
-    // Прикрепление эмодзи ошибки
-
-    this.help(msg);
+  error : (msg, text) => {
+    msg.channel.send(text);
   },
 
 
@@ -91,10 +92,10 @@ module.exports = {
    * @param {String}  name Название роли
    * @param {Number}  pos  Позиция роли
    */
-  create : function(msg, name, pos){
+  create : async function(msg, name, pos){
     name = name[0].toUpperCase() + name.slice(1);
 
-    msg.guild.roles.create({
+    const role = await msg.guild.roles.create({
       data : {
         name : name,
         mentionable : true,
@@ -106,7 +107,7 @@ module.exports = {
 
     msg.channel.send('Роль ' + name + ' создана');
 
-    return this.has(msg, name);
+    return { role : role };
   },
 
 
@@ -131,20 +132,30 @@ module.exports = {
 
 
   /**
-   * Переключение роли участнику. Если не указан 3 параметр,
-   * то выдаёт пользователю команды
+   * Переключение роли участнику.
    *
    * @param {Message} msg
-   * @param {String}  role Название роли
+   * @param {Role}    role
    * @param {Number}  user ID пользователя
    */
-  toggle : (msg, role, user) => {
+  toggle : function(msg, role, user){
+    const member = msg.guild.member(user);
 
+    if(!member)
+      return this.error(msg, 'Пользователь с ID:' + user + ' не найден');
+
+    let action = { val : 'add', text : 'выдана' };
+    if(member._roles.includes(role.id))
+      action = { val : 'remove', text : 'убрана у' }
+
+    member.roles[action.val](role, 'По требованию уполномочегонного лица');
+    msg.channel.send('Роль ' + role.name + ' ' + action.text + ' ' +
+      member.user.username + '#' + member.user.discriminator);
   },
 
 
   permission : msg =>
     msg.member.hasPermission('MANAGE_ROLES') ||
-    msg.member._roles.includes(role => role == '620194786678407181')
+    msg.member._roles.includes('620194786678407181')
 
 };
