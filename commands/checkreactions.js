92b346192eb43787ba92255ae870e7b7e1b5dfe3
BVN4 <Ruslan2804y@gmail.com>
2021-07-05 16:34:20 +0300

    release /commands/checkreactions.js

--- commands/checkreactions.js
+++ commands/checkreactions.js
@@ -1,86 +1,92 @@
-const member2name = require("../functions/member2name");
+const Buffer = require('buffer');
 
 module.exports = {
 
-    active : true,
-    name : 'checkreactions',
-    short : 'chr',
-    title : 'Сбор данных',
-    text : 'Собирает данные с выборов по указанному числу сообщений',
-    example : ' [Количество сообщений], [Канал]',
-    category : 'Выборы',
-    
-    agree_r_id : '499315437251723274',
-    disagree_r_id :'499316230172442625',
-  
-    init : function(){ return this; },
-  
-  
-    /**
-     *
-     *
-     *  @param {Message} msg
-     *  @param {Array}   params Параметры команды
-     */
-    call : async function(msg, params){
-  
-        if(!params.length && commands.list.help)
-            return commands.list.help.call(msg, [this.name]);
-  
-        if(!this.permission(msg))
-            return send.error(msg, 'У вас недостаточно прав для использования данной комманды');
-  
-        let data = {};
-        
-        const msg_count = params[0];
-        const chan_id = params[1].match(/^(<@&)?([0-9]+)(>)?$/);
+  active : true,
+  name : 'checkreactions',
+  short : 'chr',
+  title : 'Сбор данных',
+  text : 'Собирает данные с выборов по указанному числу сообщений',
+  example : ' [Количество сообщений]',
+  category : 'Выборы',
 
-        if(!chan_id) chan_id = '612280548777525249';
+  agree_r_id : '499315437251723274',
+  disagree_r_id :'499316230172442625',
+  chat_id : '612280548777525249',
 
-        const messages = await msg.channel.messages.fetch({limit: msg_count});
+  init : function(){ return this; },
 
-        for(let i = 0; i <= messages.length; i++)
-            this.getStatmentData(messages[i], data);
 
-        const json_data = JSON.stringify(data);
+  /**
+   *
+   *
+   *  @param {Message} msg
+   *  @param {Array}   params Параметры команды
+   */
+  call : async function(msg, params){
 
-        const attachment = new Discord.MessageAttachment(json_data);
+    if(!params.length && commands.list.help)
+      return commands.list.help.call(msg, [this.name]);
 
-        msg.channel.send("Сбор данных завершён!", attachment)
+    if(!this.permission(msg))
+      return send.error(msg, 'У вас недостаточно прав для использования данной комманды');
 
-    },
+    let data = {};
 
-    getStatmentData : function(msg, data){
-        const agree_r = msg.reactions.cache.get(this.agree_r_id);
-        const disagree_r = msg.reactions.cache.get(this.disagree_r_id);
+    const msg_count = params[0];
 
-        const member = msg.guild.member(msg.content.match(/^(<@!?)?([0-9]+)(>)?$/));
-        const agree_users_array = this.getReactionData(agree_r);
-        const disagree_users_array = this.getReactionData(disagree_r);
+    const messages = await msg.guild.channels.cache.get(this.chat_id).messages.fetch({limit: msg_count});
 
-        data[member.user.id] = {
-            name : member2name(member, 1), 
-            "1" : this.getReactionData(agree_r),
-            "0" : this.getReactionData(disagree_r)
-        };
-        
-    },
-    
-    getReactionData : function(reaction){
-        let data = {};
-        reaction.users.cache.each(user => {
-            data[user.id] = member2name(reaction.message.guild.member(user.id))
-        });
-        return data;
-    },
-  
-    /**
-     *  Проверка наличия прав на редактирование прав или наличие роли Оратор
-     *
-     *  @param {Message} msg
-     */
-    permission : msg =>
-        msg.member.id == '500020124515041283' ||
-        msg.member._roles.includes('613412133715312641')
-  
-  };
+    for(let message of messages.values())
+      await this.getStatmentData(message, data);
+
+    await fs.writeFile('elections.json', JSON.stringify(data), err => {
+      if(err) return console.log(err);
+    });
+
+    msg.channel.send('Сбор данных завершён!', new Discord.MessageAttachment('./elections.json'));
+  },
+
+  getStatmentData : async function(msg, data){
+    const agree_r = msg.reactions.cache.get(this.agree_r_id);
+    const disagree_r = msg.reactions.cache.get(this.disagree_r_id);
+
+    if(!disagree_r) return;
+    if(!agree_r) return;
+
+    const match = msg.content.match(/^(<@!?)?([0-9]+)(>)?$/);
+    if(!match[2]) return;
+
+    const member = msg.guild.member(match[2]);
+    if(!member) return;
+
+    data[member.user.id] = {
+      name : member2name(member, 1),
+      '1' : await this.getReactionData(agree_r),
+      '0' : await this.getReactionData(disagree_r)
+    };
+
+  },
+
+  getReactionData : async function(reaction){
+    let data = {};
+    const users = await reaction.users.fetch();
+
+    users.each(user => {
+      const member = reaction.message.guild.member(user.id);
+      if(!member) return;
+      data[user.id] = member2name(member);
+    });
+
+    return data;
+  },
+
+  /**
+   *  Проверка наличия прав на редактирование прав или наличие роли Оратор
+   *
+   *  @param {Message} msg
+   */
+  permission : msg =>
+    msg.member.id == '500020124515041283' || msg.member.id == '256114365894230018'
+
+};
